288b8668a20bafb42a7450cc4d61100858fa934e
cgspwei <cgspwei@gmail.com>
2016-08-20 18:51:19 +0800

    first commit

---
+++ 1.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2016,
+// All rights reserved.
+//
+// Author: cgspwei <cgspwei@gmail.com>
+// Created: 08/20/16
+// Description:
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    return 0;
+}
+
